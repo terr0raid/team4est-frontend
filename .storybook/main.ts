bc5995ce4f328254183319602245cb33b993d72f
terr0raid <herakles13579@gmail.com>
2023-05-22 13:54:00 +0300

    some package updates and tailwind storyjs configs

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+const config: StorybookConfig = {
+	stories: [
+		'../src/stories/**/*.mdx',
+		'../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'storybook-tailwind-dark-mode',
+		{
+			name: '@storybook/addon-styling',
+			options: {
+				postCss: true,
+			},
+		},
+	],
+	framework: {
+		name: '@storybook/nextjs',
+		options: {},
+	},
+	docs: {
+		autodocs: 'tag',
+	},
+}
+export default config
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/stories/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
+	darkMode: ['class', '[data-mode="dark"]'],
 	theme: {
 		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			colors: {
+				primary: '#ecf2fd',
+				secondary: '#030d21',
+				primaryButton: '#4882ef',
+				secondaryButton: '#000205',
+				accent: {
+					DEFAULT: '#5f92f1',
+					600: '#3d78d8',
+				},
 			},
 		},
 	},
