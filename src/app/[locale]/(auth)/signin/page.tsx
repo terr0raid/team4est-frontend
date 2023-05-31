010a920115f99f6a4067c918c07203de7231a94a
terr0raid <herakles13579@gmail.com>
2023-05-31 11:40:05 +0300

    signin page components added

--- src/app/[locale]/(auth)/signin/page.tsx
+++ src/app/[locale]/(auth)/signin/page.tsx
@@ -1,11 +1,29 @@
-import SignInCard from '@/components/cards/SignInCard'
-import { revalidatePath } from 'next/cache'
-import React from 'react'
+import CreateAccountCard from '@/components/signin/CreateAccountCard'
+import SignInCard from '@/components/signin/SignInCard'
+import SignInFallback from '@/components/fallbacks/SignInFallback'
+import React, { Suspense } from 'react'
+import { useTranslations } from 'next-intl'
+import styles from './styles.module.css'
 
 function SignIn() {
+	const t = useTranslations('auth')
 	return (
-		<div className='flex min-h-screen flex-col items-center justify-center'>
-			<SignInCard />
+		<div className={styles.main}>
+			<Suspense fallback={<SignInFallback />}>
+				<SignInCard
+					email={t('email')}
+					password={t('password')}
+					signin={t('sign_in')}
+					privacy_text={t('privacy_text')}
+					privacy_link={t('privacy_link')}
+					forgot_password={t('forgot_password')}
+					title={t('title')}
+				/>
+			</Suspense>
+			<CreateAccountCard
+				create_account={t('create_account')}
+				create_account_link={t('create_account_link')}
+			/>
 		</div>
 	)
 }
